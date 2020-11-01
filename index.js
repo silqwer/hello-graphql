245bed1ea4bcb26b09c61b35dc84517851fe8498
silqwer <silqwer@gmail.com>
2020-11-01 15:15:04 +0900

    init : project
    init graphql project

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const {buildSchema} = require('graphql');
+
+// 
+const schema = buildSchema(`
+  type Query{
+    hello : String,
+    nodejs: Int
+  }
+`);
+
+const root = {
+  hello : ()=> 'hello world',
+  nodejs : () => 20
+}
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue : root,
+  graphiql : true // gui 제공
+}));
+
+app.listen( 4000, ()=>{
+  console.log('running server port 4000');
+})
